38f4077ec742fb0dd7684722546820198a49e902
詹旺 <545578254@qq.com>
2018-08-02 09:15:26 +0800

    添加项目文件。

---
+++ github测试/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace github测试
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
